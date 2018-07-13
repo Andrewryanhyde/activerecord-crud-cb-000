60df77f5b7b97f7ec294ca319ef5547a20da8c0b
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-13 08:49:14 +0000

    automatically backed up by learn

--- db/migrate/01_create_movies.rb
+++ db/migrate/01_create_movies.rb
@@ -3,9 +3,4 @@ class CreateMovies < ActiveRecord::Migration[5.1]
     create_table :movies do |t|
       t.string :title
       t.integer :release_date
-      t.string :director 
-      t.string :lead 
-      t.boolean :in_theaters
-    end
-  end
-end
+      t.string :
